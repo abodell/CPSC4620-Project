bd3d781c2034ceea0f57c862ec2ac7213517fad7
abodell <andybodell9@gmail.com>
2023-03-28 14:09:51 -0400

    Finished the ProfitByPizza view

--- sql/CreateViews.sql
+++ sql/CreateViews.sql
@@ -11,3 +11,10 @@ ORDER BY ToppingCount DESC, Topping DESC;
 SELECT * FROM ToppingPopularity;
 
 DROP VIEW IF EXISTS ProfitByPizza;
+CREATE VIEW ProfitByPizza AS
+SELECT PizzaCrust, PizzaSize, CAST(ROUND(SUM(PizzaPrice - PizzaCost), 5) AS DECIMAL(6,2)) as Profit, DATE(MAX(OrderTime)) as LastOrderDate FROM pizza
+LEFT JOIN `order` ON PizzaOrderID = OrderID
+GROUP BY PizzaCrust, PizzaSize
+ORDER BY Profit DESC;
+
+SELECT * FROM ProfitByPizza;
